e1083e51d3b2d206aed748c53cc7497f314525e3
elorablank <elorablank@gmail.com>
2023-11-20 19:41:10 -0500

    seeing if relative path work

--- scripts/parks.js
+++ scripts/parks.js
@@ -2,12 +2,12 @@
 
 window.onload = function (_event) {
   const locationSelect = document.getElementById("locationSelect")
-  
+
   const parkTypeSelect = document.getElementById("parkTypeSelect")
-  
+
   populateLocation(locationsArray, locationSelect)
   populateParkType(parkTypesArray, parkTypeSelect)
-  
+
   const form = document.getElementById("form")
   form.onsubmit = renderCard
   form.onreset = showAll
@@ -23,13 +23,13 @@ function renderCard(event) {
   const chosenParkType = submitForm.elements.parkTypeSelect.value
 
   let html = ""
-  
+
   for (const currentPark of nationalParksArray) {
     const locationMatches = currentPark.State === chosenLocation
     const parkTypeMatches = currentPark.LocationName.includes(chosenParkType) === true
     const noLocation = chosenLocation === "no"
     const noParkType = chosenParkType === "no"
-    
+
     if (locationMatches && parkTypeMatches) {
       html += buildCard(currentPark)
     } else if (locationMatches && noParkType) {
@@ -38,13 +38,13 @@ function renderCard(event) {
       html += buildCard(currentPark)
     } else if (noLocation && noParkType) {
       html += buildCard(currentPark)
-    } 
+    }
   }
   const result = document.getElementById("resultDiv")
   result.innerHTML = html
 }
 
-function showAll (event) {
+function showAll(event) {
   event.preventDefault()
   let html = ""
   for (const currentPark of nationalParksArray) {
@@ -55,38 +55,43 @@ function showAll (event) {
 
 function buildCard(park) {
   return `
-  <div class="container">
-    <div class="card text-center">
-      <div class="card-header"></div>
-      <div class="card-body">
-        <h2 class="card-title">${park.LocationName}</h2>
-        <p class="card-text">${park.Address} , ${park.City}, ${park.State} ${park.ZipCode}</p>
+    <div class="container">
+        <div class="card text-center">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <h2 class="card-title">${park.LocationName}</h2>
+            <p class="card-text">${park.Address} , ${park.City}, ${park.State} ${park.ZipCode}</p>
+          </div>
+        <div id="footer"class="card-footer text-body-secondary">
+        ${populatePhoneNumber(park)} ${populateFax(park)} ${populateLink(park)}</div>
       </div>
-    <div id="footer"class="card-footer text-body-secondary">
-    ${populatePhoneNumber(park)} ${populateFax(park)} ${populateLink(park)}</div>
-  </div>
-</div>` 
+    </div>
+  `
 }
 
-function populateLink (parks) {
+function populateLink(parks) {
   let hasLink = parks.hasOwnProperty("Visit")
   if (hasLink === true) {
-   return `<a class="btn" role="button" href="${parks.Visit}">Visit Website</a>`
+    return `<a class="btn" role="button" href="${parks.Visit}">Visit Website</a>`
   } return ``
 }
 
-function populatePhoneNumber (parks) {
-let hasPhone = parks.Phone
-if (hasPhone === 0) {
-  return ``
-} return `<div class="d-inline btn" disabled>Call: ${hasPhone}</div>`
+function populatePhoneNumber(parks) {
+  let hasPhone = parks.Phone
+  if (hasPhone === 0) {
+    return ``
+  }
+
+  return `<div class="d-inline btn" disabled>Call: ${hasPhone}</div>`
 }
 
-function populateFax (parks) {
+function populateFax(parks) {
   let hasFax = parks.Fax
   if (hasFax === 0) {
     return ``
-  } return `<div class="d-inline btn" disabled>Fax: ${hasFax}</div>`
+  }
+
+  return `<div class="d-inline btn" disabled>Fax: ${hasFax}</div>`
 }
 
 function populateLocation(locationsArray, select) {
